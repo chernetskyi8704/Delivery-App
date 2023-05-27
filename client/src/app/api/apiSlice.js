cb0615eacab8a553f9a24d12dade109cc690b8ba
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-05-27 18:44:13 +0300

    Added vercel.json file. Removed unnecessary comments.

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
-  tagTypes: ["Category", "ProductItems"], // Для кешування данних
+  tagTypes: ["Category", "ProductItems"],
   endpoints: builder => ({}),
 });
