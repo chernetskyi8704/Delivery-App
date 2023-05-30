8c313f405ff3bfd78e8afe0591e4cd1d9842bbc3
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-05-30 20:06:25 +0300

    The event was added to the container element instead of each of the elements.

--- client/src/components/AllCategoriesItems/CategoriesItems.jsx
+++ client/src/components/AllCategoriesItems/CategoriesItems.jsx
@@ -1,19 +1,47 @@
 import classes from "./CategoriesItems.module.css";
 import { useGetAllCategoriesQuery } from "../../features/products/productsApiSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setIsCategorySelectionBlocked, setCurrentCategory, setActiveCategoryButton } from "../../features/products/productsSlice";
 import Loader from "../UI/loader/Loader";
 import CategoryItem from "../CategoryItem/CategoryItem";
 
 const AllCategoriesItems = () => {
+  const dispatch = useDispatch();
+  const router = useNavigate();
+
   const {
     data: allCategories,
     isSuccess: isAllCategoriesSuccess,
     isLoading: isAllCategoriesLoading,
   } = useGetAllCategoriesQuery();
 
+  const { orderedItems } = useSelector(
+    state => state.shoppingCart.shoppingCartSettings
+  );
+
+  const { currentCategory } = useSelector(
+    state => state.products.productsSettings
+  );
+
+  const handleChangeCategory = e => {
+    const currentCategoryId = e.target.id;
+    if (currentCategoryId) {
+      const value = e.target.getAttribute("value").toLowerCase();
+      if (orderedItems.length === 0) {
+        dispatch(setCurrentCategory(currentCategoryId));
+        dispatch(setActiveCategoryButton(currentCategoryId));
+        router(`/categories/${value}`);
+      } else if (currentCategoryId !== currentCategory) {
+        dispatch(setIsCategorySelectionBlocked(true));
+      }
+    }
+  };
+
   return (
     <section className={classes.categoriesContainer}>
       <h2>Categories:</h2>
-      <ul className={classes.categoryItems}>
+      <ul className={classes.categoryItems} onClick={handleChangeCategory}>
         {isAllCategoriesLoading && <Loader />}
         {isAllCategoriesSuccess &&
           allCategories?.map(categoryItem => {
--- client/src/components/CategoryItem/CategoryItem.jsx
+++ client/src/components/CategoryItem/CategoryItem.jsx
@@ -1,32 +1,10 @@
 import classes from "./CategoryItem.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { setCurrentCategory, setActiveCategoryButton } from "../../features/products/productsSlice";
-import { setIsCategorySelectionBlocked } from "../../features/products/productsSlice";
+import { useSelector } from "react-redux";
 
 const CategoryItem = ({ categoryItem }) => {
-  const dispatch = useDispatch();
-  const router = useNavigate();
-
-  const { currentCategory, activeCategoryButton } = useSelector(
+  const { activeCategoryButton } = useSelector(
     state => state.products.productsSettings
   );
-
-  const { orderedItems } = useSelector(
-    state => state.shoppingCart.shoppingCartSettings
-  );
-
-  const handleChangeCategory = e => {
-    const currentCategoryId = e.target.id;
-    const value = e.target.getAttribute("value").toLowerCase();
-    if (orderedItems.length === 0) {
-      dispatch(setCurrentCategory(currentCategoryId));
-      dispatch(setActiveCategoryButton(currentCategoryId));
-      router(`/categories/${value}`);
-    } else if (currentCategoryId !== currentCategory) {
-      dispatch(setIsCategorySelectionBlocked(true));
-    }
-  };
 
   return (
     <li
@@ -36,7 +14,6 @@ const CategoryItem = ({ categoryItem }) => {
       className={`${classes.categoryItem} ${
         activeCategoryButton === categoryItem._id ? classes.active : ""
       }`}
-      onClick={handleChangeCategory}
     >
       {categoryItem.categoryName}
     </li>
